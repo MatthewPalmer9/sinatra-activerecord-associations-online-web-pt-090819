d967899d4f140d1bbbc920f70abb46b55e49de2f
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-01-02 19:34:48 +0000

    Automatically backed up by Learn

--- models/cat.rb
+++ models/cat.rb
@@ -1,9 +1,3 @@
 class Cat < ActiveRecord::Base
-  # belongs_to associations MUST use a singular term.
-  # If you tried to use a pluralized form, you would be told that there was an
-  # "uninitialized constant Book::Authors". This is because Sinatra/ActiveRecord
-  # automatically infers the class name from the association name. If the
-  # association name is wrongly pluralized, then the inferred class will be wrongly pluralized too.
-  # ( :owners would look for an instance of the class Owners instead of Owner )
   belongs_to :owner
 end
